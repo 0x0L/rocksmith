ebe8bf44a89e8301aaca3b9f20bdf8d43cfd36f5
0x0L <0x0L@github.com>
2019-04-19 21:40:25 +0200

    switch to pycryptodome

--- rocksmith/crypto.py
+++ rocksmith/crypto.py
@@ -1,26 +1,36 @@
 import zlib
 
 from Crypto.Cipher import AES
-from Crypto.Util import _counter as counter
+from Crypto.Util import Counter
 
 from construct import Int32ul
 
-ARC_KEY = bytes.fromhex('C53DB23870A1A2F71CAE64061FDD0E11'
-                        '57309DC85204D4C5BFDF25090DF2572C')
+ARC_KEY = bytes.fromhex(
+    'C53DB23870A1A2F71CAE64061FDD0E11'
+    '57309DC85204D4C5BFDF25090DF2572C'
+)
 
 ARC_IV = bytes.fromhex('E915AA018FEF71FC508132E4BB4CEB42')
 
-MAC_KEY = bytes.fromhex('9821330E34B91F70D0A48CBD62599312'
-                        '6970CEA09192C0E6CDA676CC9838289D')
+MAC_KEY = bytes.fromhex(
+    '9821330E34B91F70D0A48CBD62599312'
+    '6970CEA09192C0E6CDA676CC9838289D'
+)
 
-WIN_KEY = bytes.fromhex('CB648DF3D12A16BF71701414E69619EC'
-                        '171CCA5D2A142E3E59DE7ADDA18A3A30')
+WIN_KEY = bytes.fromhex(
+    'CB648DF3D12A16BF71701414E69619EC'
+    '171CCA5D2A142E3E59DE7ADDA18A3A30'
+)
 
-PRF_KEY = bytes.fromhex('728B369E24ED0134768511021812AFC0'
-                        'A3C25D02065F166B4BCC58CD2644F29E')
+PRF_KEY = bytes.fromhex(
+    '728B369E24ED0134768511021812AFC0'
+    'A3C25D02065F166B4BCC58CD2644F29E'
+)
 
-CONFIG_KEY = bytes.fromhex('378B9026EE7DE70B8AF124C1E3097867'
-                           '0F9EC8FD5E7285A86442DD73068C0473')
+CONFIG_KEY = bytes.fromhex(
+    '378B9026EE7DE70B8AF124C1E3097867'
+    '0F9EC8FD5E7285A86442DD73068C0473'
+)
 
 
 def pad(data, blocksize=16):
@@ -32,7 +42,8 @@ def aes_bom():
 
 
 def aes_sng(key, ivector):
-    ctr = counter._newBE(b'', b'', ivector, allow_wraparound=False)
+    iv = int.from_bytes(ivector, 'big')
+    ctr = Counter.new(128, initial_value=iv, allow_wraparound=False)
     return AES.new(key, mode=AES.MODE_CTR, counter=ctr)
 
 
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
+
 def readme():
     with open('README.md') as f:
         return f.read()
+
 
 setup(
     name='rocksmith',
@@ -20,13 +22,10 @@ setup(
     author_email='0x0L@github.com',
     license='MIT',
     packages=['rocksmith'],
-    entry_points = {
+    entry_points={
         'console_scripts': ['pyrocksmith=rocksmith.command_line:main'],
     },
-    install_requires=[
-        'pycrypto',
-        'construct==2.8.10'
-    ],
+    install_requires=['pycryptodome', 'construct==2.8.10'],
     include_package_data=True,
     zip_safe=False
 )
